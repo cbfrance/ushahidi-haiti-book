4adf24317496d1f41b0ea8be331bdb0c19e05461
Chris Blow <unthinkingly@gmail.com>
2010-05-15 21:18:13 -0700

    use document.generate instead of builder

--- csv2pdf.rb
+++ csv2pdf.rb
@@ -4,23 +4,18 @@ require 'prawn'
 
 Prawn.debug = true
 
-builder = Prawn::DocumentBuilder.new
-font = "Times"
+Prawn::Document.generate("book.pdf") do  
+  unless ARGV.length == 1
+    puts "Usage: ruby csv2pdf.rb your_ushahidi_export_file.csv > output.pdf\n"
+    exit
+  end
 
-unless ARGV.length == 1
-  puts "Usage: ruby csv2pdf.rb your_ushahidi_export_file.csv > output.pdf\n"
-  exit
+  input_file = ARGV[0]
+
+  f = File.open(input_file, "r")
+  f.each_line { |line|
+    words = line.split('%t')
+    text(words[0].tr_s('"', '').strip)
+    start_new_page
+  }
 end
-
-input_file = ARGV[0]
-
-f = File.open(input_file, "r")
-f.each_line { |line|
-  words = line.split('%t')
-  builder.text(words[0].tr_s('"', '').strip)
-  builder.start_new_page
-}
-
-
-document = builder.compile
-document.render_file("book.pdf")

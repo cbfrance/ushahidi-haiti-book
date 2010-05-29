cf4ae7726325a3b8b03f35a0ac760b9b3fa35bfe
Chris Blow <unthinkingly@gmail.com>
2010-05-28 21:55:50 -0700

    removed CSV support, starting to add yaml support.

--- ushahidi2pdf.rb
+++ ushahidi2pdf.rb
@@ -6,9 +6,6 @@ require "httparty"
 require "ap"
 require 'crack'
 
-# config
-instance_address= "http://haiti.ushahidi.com/api?task=incidents&by=all"
-
 class Book
   def initialize
     @pdf = Prawn::Document.new(:page_layout => :landscape)
@@ -39,47 +36,36 @@ end
 
 class Cache
   def initalize
-    format= ARGV[0]
+    @format= ARGV[0]
   end
-  def parse(format)
-    if format =~ /.json$/
-      jsonfile= File.open("cache.txt", "r")
-      results= jsonfile.read
-      parsed_results= Crack::JSON.parse(results)
-      jsonfile.close
-      @incidents = parsed_results['payload']['incidents']
-    elsif(format =~ /.csv$/ )
-      filename = ARGV[0]
-
-      @pdf.bounding_box([@x_pos, @y_pos], :width => 300, :height => 400) do  
-        csv = File.open(filename, "r")        
-        f.each_line { |line|
-          words = line.split('%t')
-          if words[0].tr_s('"', '').strip != ""
-            pdf.text(words[0].tr_s('"', '').strip.capitalize)
-          end
-          pdf.start_new_page
-        }
-      end
-    end
-    
+  def parse(format = "json")
+    jsonfile= File.open("cache.json", "r")
+    results= jsonfile.read
+    parsed_results= Crack::JSON.parse(results)
+    jsonfile.close
+    @incidents = parsed_results['payload']['incidents']
   end
     
-  end
   def full?
-    File.exist?("cache.txt")
+    File.exist?("cache.json")
   end
+   
+    
   def fill_from_url
     p "reading instance API ..."  
+    instance_address= "http://haiti.ushahidi.com/api?task=incidents&by=all"
     response= HTTParty.get(instance_address)
-    jsonfile= File.new("cache.txt", "w")
+    jsonfile= File.new("cache.json", "w")
+    p "Saving yaml"
+    yamlfile= jsonfile.to_yaml
+    p yamlfile
+    p "success"
     data = response.body
     puts data
     jsonfile.write(data)
+
+    puts "cache written"
     jsonfile.close
-  end
-  def fill_from_csv
-    
   end
 end
 
@@ -90,29 +76,12 @@ end
 
 book=Book.new
 cache=Cache.new
-
-if ARGV[0] =~ /.csv$/
-  book.typeset  
-  cache.fill_from_csv
-  cache.parse_csv
-  
-elsif ARGV[0] =~ /.json$/ 
-  pdf.bounding_box([x_pos, y_pos], :width => 300, :height => 400) do  
-    @incidents.each do |incident|
-      pdf.text incident['incidenttitle']
-      pdf.start_new_page
-    end
-  end
-
-# ============
-# = API CALL =
-# ============  
+if cache.full?
+  texts= cache.parse("json")
+  # book.bind(texts)
+  p "binding"
+  # book.print
+  p "printing"
 else
-  if cache.full?
-    text= cache.parse_json
-    book.bind(texts)
-    book.print
-  else
-    cache.fill_from_url
-  end
+  cache.fill_from_url
 end

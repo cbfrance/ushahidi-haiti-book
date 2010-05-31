75971777acf01187d871370fb2ef75a52ca1a98a
Chris Blow <unthinkingly@gmail.com>
2010-05-31 03:09:24 -0700

    extracted caching/crawler classes

--- ushahidi2pdf.rb
+++ ushahidi2pdf.rb
@@ -1,88 +1,104 @@
 require 'rubygems'
-$LOAD_PATH.unshift("/Users/davidnolen/development/ruby/prawn/lib")
+$LOAD_PATH.unshift("/Users/chris/git/prawn/lib")
 require 'prawn'
 require 'json'
 require "rubygems"
 require "httparty"
-#require "ap"
 require 'crack'
 
-class Book
-  def initialize
-    @pdf = Prawn::Document.new(:page_layout => :landscape)
-    Prawn.debug = true
-  end
-  def typeset
-    @pdf.font_families.update("Hoefler" => { :normal => "Hoefler Text.dfont" })
-    @pdf.font("Hoefler", :style => :normal)
-    @pdf.font_size 15    
-    @x_pos = ((@pdf.bounds.width / 2) - 150) 
-    @y_pos = ((@pdf.bounds.height / 2) + 100)
-  end
-  def bind(texts)
-    self.typeset
-    #draw the box, add reports
-    @pdf.bounding_box([@x_pos, @y_pos], :width => 300, :height => 400) do  
-      texts.each do |i|
-        @pdf.text((i['incident']['incidentdescription']).gsub(/IDUshahidi:\W+\d+/, ''))
-        @pdf.start_new_page
-      end    
+INSTANCE= "http://haiti.ushahidi.com/api?task=incidents&by=all"
+
+module PrintingPress
+  class Book
+    def initialize
+      @book = Prawn::Document.new
+      Prawn.debug = true
+      @x_pos = ((@book.bounds.width / 2) - 150) 
+      @y_pos = ((@book.bounds.height / 2) + 100)
+      
+    end
+    def typeset_header
+      @book.font_families.update("Hoefler" => { :normal => "Hoefler Text.dfont" })
+      @book.font("Hoefler", :style => :normal)
+      @book.font_size 15
+    end
+  
+    def typeset_date
+      @book.font_size 10
+      @book.font("Courier")
+    end
+  
+    def typeset_latlong
+      @book.font_size 6
+    end
+  
+    def bind(incidents)
+      typeset_date
+      #draw the box, add reports
+      incidents.each do |i|
+        @book.bounding_box([@x_pos, @y_pos], :width => 300, :height => 400) do  
+          @book.text((i['incident']['incidenttitle']))
+          @book.text("----------------------------------------------")
+          @book.text((i['incident']['incidentdescription']).gsub(/IDUshahidi:\W+\d+/, ''))
+        end
+        
+        
+        @book.text(i['incident']['incidentdate'])
+        @book.text(i['incident']['locationlatitude'])
+        @book.text(i['incident']['locationlongitude'])
+        @book.start_new_page
+      end
+    end
+    def print
+      @book.render_file("book.pdf")
+      `open book.pdf`
     end
   end
-  def print
-    @pdf.render_file("book.pdf")
-    `open book.pdf`
-  end
-end
 
-class Cache
-  def initalize
-    @format= ARGV[0]
-  end
-  def parse(format = "json")
-    jsonfile= File.open("cache.json", "r")
-    results= jsonfile.read
-    parsed_results= Crack::JSON.parse(results)
-    jsonfile.close
-    @incidents = parsed_results['payload']['incidents']
-  end
+  class Cache
+    def full?
+      File.exist?("cache.json")
+    end
     
-  def full?
-    File.exist?("cache.json")
+    def read
+      p "... reading cache"
+      jsonfile= File.open("cache.json", "r")
+      results= jsonfile.read
+      parsed_results= Crack::JSON.parse(results)
+      jsonfile.close
+      p "... incidents loaded"
+      return parsed_results['payload']['incidents']
+    end
+  
+    def write(data)
+      jsonfile= File.new("cache.json", "w")
+      jsonfile.write(JSON.pretty_generate(Crack::JSON.parse(data)))
+      jsonfile.close
+      p "... cache written"
+    end
   end
-   
-    
-  def fill_from_url
-    p "reading instance API ..."  
-    instance_address= "http://haiti.ushahidi.com/api?task=incidents&by=all"
-    response= HTTParty.get(instance_address)
-    jsonfile= File.new("cache.json", "w")
-    p "Saving yaml"
-    yamlfile= jsonfile.to_yaml
-    p yamlfile
-    p "success"
-    data = response.body
-    puts data
-    jsonfile.write(JSON.pretty_generate(Crack::JSON.parse(data)))
 
-    puts "cache written"
-    jsonfile.close
+  class Crawler
+    def crawl(instance)
+      p "crawling around on the web..."
+      return HTTParty.get(INSTANCE).body
+      # read the id of the last item
+      # be nice
+      # sleep 1
+      # start another query from where we left off
+      # remove duplicates
+    end
   end
 end
 
+book= PrintingPress::Book.new
+crawler=PrintingPress::Crawler.new
+cache=PrintingPress::Cache.new
 
-# ==================
-# = printing press =
-# ==================
-
-book=Book.new
-cache=Cache.new
-if cache.full?
-  texts= cache.parse("json")
-  # book.bind(texts)
-  p "binding"
-  # book.print
-  p "printing"
-else
-  cache.fill_from_url
+unless cache.full?
+  cache.write(crawler.crawl(INSTANCE))
 end
+
+book.bind(cache.read)
+p "printing!"
+book.print
